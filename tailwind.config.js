7d8aff082799c327c0cb64e18d6c82632a0a2d74
MUTHUAKALYA <muthuakalyaa@gmail.com>
2023-10-10 06:02:20 -0700

    design:white Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
+  content: ["./index.html","./light.html", "./myprofile.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
