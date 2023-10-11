eb2a0510abd56e61772eb5788ecbaad67fdc56dc
MUTHUAKALYA <muthuakalyaa@gmail.com>
2023-10-11 01:12:06 -0700

    added cibilscore & bankbalance pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./light.html", "./myprofile.html",
+  content: ["./index.html","./light.html", "./myprofile.html","./bankbalance.html","./cibilscore.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
