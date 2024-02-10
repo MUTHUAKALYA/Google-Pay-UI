a061fbe64255346919c7c55b8fddc35dd9267c73
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-02-11 00:16:12 +0530

    configuring  bankbalancelight and cibilscorelight page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./light.html", "./myprofile.html","./bankbalance.html","./cibilscore.html",
+  content: ["./index.html","./light.html", "./myprofile.html","./bankbalance.html","./cibilscore.html","./cibilscorelight.html","./bankbalancelight.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
